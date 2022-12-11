a335a9727c673b9ff6c72670bf0b37479abc3cd1
gem1n1-youngjae <spawn12358@gmail.com>
2022-12-11 23:40:09 +0900

    🐛 fix: change wrong images

--- src/assets/images/gomem/secretGirl/epic/index.ts
+++ src/assets/images/gomem/secretGirl/epic/index.ts
@@ -1,9 +1,9 @@
-import 가수_비밀_소녀 from "./가수_비밀_소녀 .png";
+import 가수_비밀_소녀 from "./가수_비밀_소녀.png";
 import 외계인에게_잡힌_비밀소녀 from "./외계인에게_잡힌_비밀소녀.png";
 import 지수소녀 from "./지수소녀.png";
 
 export const secretGirlEpicImages = {
   가수_비밀_소녀,
   외계인에게_잡힌_비밀소녀,
-  지수소녀,
+  지수소녀
 };
