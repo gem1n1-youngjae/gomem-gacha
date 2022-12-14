ec656b32500916e80dc8f16c1b5e4994e3dd8abb
gem1n1-youngjae <spawn12358@gmail.com>
2022-12-14 22:46:44 +0900

    🐛 fix(card goeme name): fix gomem name text line break bug

--- src/assets/images/gomem/hidden/kimchimandu/index.ts
+++ src/assets/images/gomem/hidden/kimchimandu/index.ts
@@ -1,7 +1,7 @@
 import 북기만두 from "./북기만두.png";
-import 히든김치만두번영택사스가 from "./히든김치만두번영택사스가.png";
+import 히든김치만두 from "./히든김치만두.png";
 
 export const kimchimandu = {
   북기만두,
-  히든김치만두번영택사스가
+  히든김치만두
 };
