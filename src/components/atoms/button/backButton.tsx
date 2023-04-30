43cac1cb931995f0f2c2c4b3b004a861624c66d9
gem1n1-youngjae <spawn12358@gmail.com>
2023-04-30 18:17:47 +0900

    ✨ feat(sound): update back button sound

--- src/components/atoms/button/backButton.tsx
+++ src/components/atoms/button/backButton.tsx
@@ -2,13 +2,17 @@ import { useNavigate } from "react-router-dom";
 
 import { StyledBackButton } from "./backbutton.style";
 
+import { useStarSoundHook } from "assets/sounds/hooks";
+
 export const BackButton = ({ className }: { className?: string }) => {
   const navigate = useNavigate();
+  const { clickButton } = useStarSoundHook();
   return (
     <StyledBackButton
       className={className}
       onClick={() => {
         navigate(-1);
+        clickButton();
       }}
     />
   );
