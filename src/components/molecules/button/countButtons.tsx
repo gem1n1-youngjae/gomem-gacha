6948b51e19c8e891b2f0a5a3d1d9234114af9138
gem1n1-youngjae <spawn12358@gmail.com>
2023-05-02 23:50:26 +0900

    ✨ feat(button): update count buttons

---
+++ src/components/molecules/button/countButtons.tsx
@@ -0,0 +1,71 @@
+import { useCallback, useState } from "react";
+import styled from "styled-components";
+import { PlayFunction } from "use-sound/dist/types";
+
+import { PopoutButton } from "components/atoms";
+
+const CountButtonsWrapper = styled.div`
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  height: 150px;
+  z-index: 99;
+`;
+
+const StyledPopoutButton = styled(PopoutButton)`
+  width: max-content;
+  height: max-content;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  & > .buttonTopSide {
+    padding: 20px 25px;
+    font-size: 18px;
+  }
+`;
+
+export const CountButtons = ({
+  clickButton
+}: {
+  clickButton?: PlayFunction;
+}) => {
+  const defaultCount =
+    Number(JSON.parse(window.localStorage.getItem("count"))) ?? 0;
+  const [number, setNumber] = useState(defaultCount);
+
+  const onIncrease = useCallback(() => {
+    setNumber(number + 1);
+    window.localStorage.setItem("count", String(number + 1));
+  }, [number]);
+
+  const onReset = useCallback(() => {
+    setNumber(0);
+    window.localStorage.setItem("count", String(0));
+  }, []);
+
+  return (
+    <CountButtonsWrapper>
+      <StyledPopoutButton
+        onClick={() => {
+          onReset();
+          clickButton?.();
+        }}
+      >
+        re
+      </StyledPopoutButton>
+      <StyledPopoutButton
+        onClick={() => {
+          onIncrease();
+          clickButton?.();
+        }}
+      >
+        {number}
+      </StyledPopoutButton>
+    </CountButtonsWrapper>
+  );
+};
